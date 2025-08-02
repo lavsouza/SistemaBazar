d127561760915ef3d32e4b254e28b408369cc675
Lucas André <lucascoder14@gmail.com>
2025-08-02 18:27:01 -0300

    Configuração do projeto

---
+++ src/main/java/web2/sistemabazar/SistemaBazarApplication.java
@@ -0,0 +1,12 @@
+package web2.sistemabazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBazarApplication.class, args);
+	}
+}
---
+++ src/test/java/web2/sistemabazar/SistemaBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package web2.sistemabazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
